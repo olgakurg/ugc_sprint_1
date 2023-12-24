59ea63724a7e3225d76047745fb7aa549f6edd09
olgakurg <olgakurg@gmail.com>
2023-12-24 16:51:18 +0800

    add topic creation

--- src/db/kafka_settings.py
+++ src/db/kafka_settings.py
@@ -1,24 +1,26 @@
+import logging
+from typing import Dict, Type
 
 from core.config import Settings
 from kafka3.admin import KafkaAdminClient, NewTopic
 from models import MovieProgress, MovieRes, ClickElement, FilterQuery, PageDuration
 
-kafka_topics = {
+kafka_topics: Dict[Type, str] = {
     MovieProgress: "movies_progress",
     MovieRes: "movie_resolution",
     ClickElement: "click_element",
     FilterQuery: "filter_query",
     PageDuration: "page_duration",
-
 }
 
 
-def create_topic(topic_name, settings: Settings):
-    admin_client = KafkaAdminClient(bootstrap_servers=f'{settings.kafka_host}:{settings.kafka_host}')
+def create_topic(topic_name: str, settings: Settings) -> None:
+    admin_client = KafkaAdminClient(bootstrap_servers=f'{settings.kafka_host}:{settings.kafka_port}')
     topic = NewTopic(name=topic_name, num_partitions=1, replication_factor=1)
     admin_client.create_topics(new_topics=[topic], validate_only=False)
+    logging.info(f'Created topic {topic_name}')
 
 
-def create_all_topics(kafka_topics, settings: Settings):
+def create_all_topics(kafka_topics: Dict[Type, str], settings: Settings) -> None:
     for topic_name in kafka_topics.values():
         create_topic(topic_name, settings)
