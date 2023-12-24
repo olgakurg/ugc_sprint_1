760a84912c917fccc6ced29274f57aebffd55ef6
olgakurg <olgakurg@gmail.com>
2023-12-24 09:56:42 +0800

    fix copy start-kafka.sh for wsl

--- dockerfiles/kafka-build/start-kafka.sh
+++ dockerfiles/kafka-build/start-kafka.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 /kafka/bin/kafka-storage.sh format -t 76BLQI7sT_ql1mBfKsOk9Q -c /kafka/config/kraft/server.properties --ignore-formatted
-
 /kafka/bin/kafka-server-start.sh /kafka/config/kraft/server.properties
